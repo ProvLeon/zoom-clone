ea1e8a1f275fcbf2162bc9fadf6585a4b737ae3b
ProvLeon <providence.leonard@gmail.com>
2024-05-02 00:31:25 +0000

    done

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -64,12 +64,12 @@ const MeetingRoom = () => {
         </div>
       </div>
       {/* video layout and call controls */}
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
-        <CallControls onLeave={() => router.push(`/`)} />
-
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-1 px-2">
+        <div className='flex'><CallControls onLeave={() => router.push(`/`)} /></div>
+        <div className='hidden sm:block'>
         <DropdownMenu>
           <div className="flex items-center">
-            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-2 py-2 sm:px-4 sm:py-2 hover:bg-[#4c535b]">
               <LayoutList size={20} className="text-white" />
             </DropdownMenuTrigger>
           </div>
@@ -90,11 +90,61 @@ const MeetingRoom = () => {
         </DropdownMenu>
         <CallStatsButton />
         <button onClick={() => setShowParticipants((prev) => !prev)}>
-          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-2 py-2 sm:px-4 sm:py-2 hover:bg-[#4c535b]">
             <Users size={20} className="text-white" />
           </div>
         </button>
         {!isPersonalRoom && <EndCallButton />}
+        </div>
+        <div className='sm:hidden'>
+          <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-2 py-2 sm:px-4 sm:py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {/* {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => ( */}
+            <div>
+                <DropdownMenuItem>
+                <DropdownMenu>
+                  <div className="flex items-center">
+                    <DropdownMenuTrigger className="flex cursor-pointer rounded-2xl bg-[#19232d] px-2 py-2 sm:px-4 sm:py-2 hover:bg-[#4c535b] hover:text-gray-400">
+                      <LayoutList size={20} className="text-white" /> <p className='hover:text-gray-400 ml-7'>Layout</p>
+                    </DropdownMenuTrigger>
+                  </div>
+                  <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+                    {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                      <div key={index}>
+                        <DropdownMenuItem
+                          onClick={() =>
+                            setLayout(item.toLowerCase() as CallLayoutType)
+                          }
+                        >
+                          {item}
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator className="border-gray-200" />
+                      </div>
+                    ))}
+                  </DropdownMenuContent>
+                  </DropdownMenu>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-gray-200" />
+                {/* <DropdownMenuItem>
+                  <div className='cursor-pointer text-gray-400 rounded-2xl bg-[#19232d] px-2 py-2 sm:px-4 sm:py-2 hover:bg-[#4c535b]'>
+                    <CallStatsButton />
+                  </div>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-gray-200" /> */}
+                <DropdownMenuItem>
+                  {!isPersonalRoom && <EndCallButton />}
+                </DropdownMenuItem>
+              </div>
+          </DropdownMenuContent>
+
+          </DropdownMenu>
+
+        </div>
       </div>
     </section>
   );
